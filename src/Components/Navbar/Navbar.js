ea32667d12c8870f22f94fd2ba2f26255f19c8f6
almahfuz <almahfuz0327@gmail.com>
2022-11-29 20:20:25 +0600

    edited sign out

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -54,9 +54,8 @@ export default function Navbar2() {
                                     <NavLink to="/dashboard"
                                         className={({ isActive }) => isActive ? 'text-primary-color block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-color md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' : `block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-color md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Dashboard</NavLink>
                                 </li>
-                                <li>
-                                    <NavLink to='/signOut'
-                                        onClick={handleSignOut}
+                                <li onClick={handleSignOut}>
+                                    <NavLink
                                         className={({ isActive }) => isActive ? 'text-primary-color block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-color md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' : `block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary-color md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>Sign Out</NavLink>
                                 </li>
                             </>
